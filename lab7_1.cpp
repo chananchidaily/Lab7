bff2747bc7346e5f989fd10a8c9d4c6b32cedb6a
tonkhaooo <chananchida_tha@cmu.ac.th>
2022-12-13 16:05:58 +0700

    lab7_1 completed laew_kaaa

--- lab7_1.cpp
+++ lab7_1.cpp
@@ -34,8 +34,17 @@ string func3(string x){
 }
 
 int main(){
-    cout >> "Input text: "
-    cout >> "Reversed text: "
-    cout >> "Palindrome: "
-    return 0;
+	string x ;
+    cout << "Input text: ";
+	cin >> x;
+	
+    cout << "Reversed text: " << func1(x) << "\n" ;
+	
+	if(func2(func1(x))==func2(x)){ 
+		cout << "Palindrome: "<< "Yes";
+	}else{
+		cout << "Palindrome: "<< "No" ;
+	}
+	return 0;
+
 }
